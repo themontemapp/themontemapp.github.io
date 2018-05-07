416a36da5ebaeb713b9f1ed1b580cdb7577d9c02
Rune Jacobsen <slashrune@gmail.com>
2018-05-07 12:00:51 +0200

    4 - Added slider main component

--- themontemapp/src/Components/App/App.js
+++ themontemapp/src/Components/App/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Slider from '../Slider/Slider';
 
 class App extends Component {
   render() {
     return (
       <div>
         This is the app component
+        <Slider />
       </div>
     );
   }
---
+++ themontemapp/src/Components/Slider/Slider.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Slider.css';
+
+class Slider extends Component {
+  render() {
+    return (
+      <div>
+        This is the slider component
+      </div>
+    );
+  }
+}
+
+export default Slider;
