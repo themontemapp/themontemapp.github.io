dd9d90ea8444961f0f205720f00cc7a462cd6250
Rune Jacobsen <slashrune@gmail.com>
2018-05-09 08:34:53 +0200

    12 - Added datastructure for chat + example

--- themontemapp/src/Components/App/App.js
+++ themontemapp/src/Components/App/App.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import { getData } from '../../Data/dataStructure'
+
 class App extends Component {
   render() {
+
+    console.log(getData());
+
     return (
       <div>
-        This is the app component
+        {/* This is the app component¨ */}
+        {
+          getData().conversation.map((con, i) => {
+            return (
+              <div key={i}>
+                <div>
+                  {con.bot.prompts.map(prompt =>
+                    <p key={prompt}>{prompt}</p>
+                  )}
+                </div>
+                <div>
+                  {con.user.options.map(opt =>
+                    <p style={{ textAlign: 'right' }} key={opt.value}>{opt.answer}</p>
+                  )}
+                </div>
+              </div>
+            )
+          })
+        }
       </div>
-    );
+    )
   }
 }
 
--- themontemapp/src/Data/dataStructure.js
+++ themontemapp/src/Data/dataStructure.js
@@ -1,5 +1,70 @@
-export const getData = () => (
-    {
-        
-    }
-)
+export const getData = () => ({
+    conversation: [
+        {
+            type: 'button',
+            bot: {
+                prompts: ['How are you doing', 'And something about Monti', 'somethin more about med'],
+                responds: 'That sounds something'
+            },
+            user: {
+                options: [
+                    {
+                        answer: 'Fine',
+                        value: 1
+                    },
+                    {
+                        answer: 'Alright',
+                        value: 2
+                    }
+                ],
+                answer: null
+            }
+        },
+        {
+            type: 'slider',
+            bot: {
+                prompts: ['How relaxed do you feel right now?'],
+                responds: []
+            },
+            user: {
+                options: [
+                    {
+                        answer: 'Not relaxed',
+                        value: 1
+                    },
+                    {
+                        answer: 'Slightly relaxed',
+                        value: 2
+                    },
+                    {
+                        answer: 'Relaxed',
+                        value: 3
+                    },
+                    {
+                        answer: 'Very relaxed',
+                        value: 4
+                    }
+                ],
+                answer: {
+                    answer: '',
+                    value: null
+                }
+            }
+        }
+    ],
+    conversationHistory: [
+        {
+            time: null,
+            day: null,
+            conversations: [
+                {
+                    time: 'hh:mm:ss',
+                    date: 'dd-mm-yy',
+                    conversation: {
+
+                    }
+                }
+            ]
+        }
+    ]
+})
