970e60e04c59eeeba4e5d16696de5ed29c369b74
Mads Borup <madsborpe@gmail.com>
2018-05-14 17:53:00 +0200

    19. First commit

--- themontemapp/src/Components/App/App.js
+++ themontemapp/src/Components/App/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import Button from '../Button/Button';
 
 class App extends Component {
+
+  onButtonPress = (buttonValue) => {
+    console.log(buttonValue);
+  }
+
   render() {
     return (
       <div>
         This is the app component
+        <Button text="test test test" returnValue="Ja" onClick={this.onButtonPress} imageSource="/assets/img/icon-right.svg"></Button>
       </div>
     );
   }
